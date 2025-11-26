83193b83a7201059a05a33a14851c16ceeee7d5d
SimonThoustrup <41516634+SimonThoustrup@users.noreply.github.com>
2025-11-26 14:57:49 +0100

    Update expense_claim_print.py

--- frappe_expense_claim_print/api/expense_claim_print.py
+++ frappe_expense_claim_print/api/expense_claim_print.py
@@ -6,12 +6,14 @@ import io
 
 @frappe.whitelist()
 def get_expense_claim_with_attachments_pdf(name):
+    # Base PDF
     html = frappe.get_print("Expense Claim", name)
-    claim_pdf = get_pdf(html)
+    base_pdf = get_pdf(html)
 
     merger = PdfMerger()
-    merger.append(io.BytesIO(claim_pdf))
+    merger.append(io.BytesIO(base_pdf))
 
+    # Attachments
     attachments = frappe.get_all(
         "File",
         filters={"attached_to_doctype": "Expense Claim", "attached_to_name": name},
@@ -27,14 +29,15 @@ def get_expense_claim_with_attachments_pdf(name):
         if not fn.endswith(".pdf"):
             encoded = base64.b64encode(content).decode()
             html = f"""
-            <html><body style="margin:0;padding:0">
-            <img src="data:image/*;base64,{encoded}" style="width:100%; height:auto;">
-            </body></html>
+                <html><body style="margin:0;padding:0">
+                <img src="data:image/*;base64,{encoded}" style="width:100%;height:auto;">
+                </body></html>
             """
             content = get_pdf(html)
 
         merger.append(io.BytesIO(content))
 
+    # Output PDF
     out = io.BytesIO()
     merger.write(out)
     merger.close()

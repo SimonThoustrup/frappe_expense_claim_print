fe36aa1cb941ac06e91790d753abd53ce9369cbf
SimonThoustrup <41516634+SimonThoustrup@users.noreply.github.com>
2025-11-26 15:16:01 +0100

    Update hooks.py

--- frappe_expense_claim_print/hooks.py
+++ frappe_expense_claim_print/hooks.py
@@ -4,7 +4,7 @@ app_publisher = "Simon Thoustrup"
 app_description = "Adds a Print with Attachments button for Expense Claims"
 app_email = "simon@thoustrup.dk"
 app_license = "MIT"
-app_include_js = "public/js/frappe_expense_claim_print.bundle.js"
+app_include_js = "frappe_expense_claim_print.bundle.js"
 
 doctype_js = {
     "Expense Claim": "public/js/expense_claim.js"
